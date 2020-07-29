68c760faad0fe98c62d523cb886d4f02de65f21a
alejandra <alejandra.colicheo17@gmail.com>
2020-07-29 18:14:47 -0400

    buscando archivos.md y ruta absoluta

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-/* module.exports = () => {
-  // ...
-}; */
+//const fetch = require("fetch")
+//const fetchUrl = fetch.fetchUrl;
 
-const fetch = require("fetch")
-const fetchUrl = fetch.fetchUrl;
+const path = require ("path");
+const fs = require ("fs");
+let file = process.argv[2]; // Toma lo que se le da desde consola
+file = path.resolve(file);  // Convierte a ruta absoluta
+file = path.normalize(file); // simplifica nuestras rutas
 
+console.log("file",file);
 
-
-
-
-const getHttpStatus = (url) => {
-  
+const getFilemd = (file) => {
   return new Promise((resolve, reject) => {
-    
-    fetchUrl(url, (error, meta, body) => {
-      if(error){
-        reject(error);
-      }else{
-        resolve(meta.status);
-      } 
-    });
-
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err)
+      } else {
+        resolve(data)
+      }
+        //console.log(reject);
+      })
   })
 }
 
-let url = "https://www.googlewwwwwww.com/"
-getHttpStatus(url)
-.then(res =>{
-  console.log("El estado de", url, "es:", res);
-})
-.catch(err =>{
-  console.log(err.code);
-});
+if (path.extname(file) === '.md') {
+   getFilemd(file)
+    
+    .then((fileData) => {
+      console.log(fileData);
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+} else {
+    console.log('Por favor, introduce una ruta que contenga un archivo .md')
+}
+  
+  
+  
 
-console.log("El programa sigue");
 
 
 
