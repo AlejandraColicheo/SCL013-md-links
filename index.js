840951977ce815049de29fe380393cee5c2d4f78
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-07-29 12:53:20 -0400

    New Promise y estado de los link

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+const fetch = require("fetch")
+const fetchUrl = fetch.fetchUrl;
+
+
+
+
+
+const getHttpStatus = (url) => {
+  
+  return new Promise((resolve, reject) => {
+    
+    fetchUrl(url, (error, meta, body) => {
+      if(error){
+        reject(error);
+      }else{
+        resolve(meta.status);
+      } 
+    });
+
+  })
+}
+
+let url = "https://www.googlewwwwwww.com/"
+getHttpStatus(url)
+.then(res =>{
+  console.log("El estado de", url, "es:", res);
+})
+.catch(err =>{
+  console.log(err.code);
+});
+
+console.log("El programa sigue");
+
 
 
