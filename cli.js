200d569ef8f49a199c8c3fc3ad0510933e86db1e
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-08-05 10:13:10 -0400

    Modificando funcion

--- cli.js
+++ cli.js
@@ -1,76 +1,20 @@
-const readFile = require('./index');
-const marked = require('marked');
-const fetch = require('node-fetch');
-const file = process.argv[2];
-const path = require('path');
-const absolutePath = path.normalize(path.resolve(file)); // normalize() arregla la ruta. resolve() la hace absoluta
-const colors = require('colors');
 
-function detectedMd(absolutePath) { // Función para detectar archivos tipo .md
-  if (path.extname(absolutePath) === '.md') {
-    getLinks();
-  } else {
-    console.log('error. Ingrese un archivo .md');
-  }
+const fs = require('fs');
+
+module.exports = () => {
+
 };
 
-function getLinks() { // Función para obtener arreglo de todos los links
-  let printLinks = new Promise((resolve, reject) => {
-    readFile.readFile(absolutePath)
-      .then(datos => {
-        let renderer = new marked.Renderer();
-        let links = [];
-        renderer.link = function (href, title, text) {
-          links.push({
-            href: href,
-            text: text,
-            file: absolutePath,
-          });
-        }; //console.log("renderer.link",renderer.link);
-        marked(datos, {
-          renderer: renderer
-        });
-        links = filterHttp(links); // Filtrar por prefijo http
-        links = stateLinks(links)
-        return resolve(links)
-      })
-      .catch(err => {
-        (console.log(err));
-      })
+const readFile = (path) => { // Función para leer archivo
+  let content = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
   })
-  return printLinks
+  return content
 }
 
-function stateLinks(links) { // Función que filtra por estado de links
-  links.forEach(element => {
-    fetch(element.href)
-      .then(response => {
-        if (response.status === 200) {
-          console.log(colors.blue('(text ' + element.text + '\n'), colors.yellow('href: ' + element.href + '\n'), ('file: '+ element.file + '\n'), colors.green('Estado: ' + response.status + ')'));
-          //console.log(chalk.yellow('href: ' + linksArry ));
-              console.log((colors.blue("Links:")), element.href)
-              //console.log("newArry",newArry);
-        } else {
-          console.log(colors.blue('(text ' + element.text + ''), colors.yellow('href: ' + element.href), ('file: '+ element.file), colors.red('Estado: ' + response.status + ')'));
-        }
-      })
-      
-      .catch(error => 
-        console.log(colors.red('Link con error: ' + element.href)))
-        
-  });
-  console.log(colors.green("Total de links analizados " +links.length));
-};
-
-function filterHttp(links) { // Función que filtra por prefijo http de links
-  let filterHttp = [];
-  links.map((element) => {
-    let prefix = element.href.substring(0, 4);
-    if (prefix == 'http') {
-        filterHttp.push(element);
-    }
-  })
-  return filterHttp;
-  
-};
-detectedMd(absolutePath);
+module.exports.readFile = readFile;
--- index.js
+++ index.js
@@ -1,20 +1,71 @@
+const readFile = require('./cli');
+const marked = require('marked');
+const fetch = require('node-fetch');
+const file = process.argv[2];
+const path = require('path');
+const absolutePath = path.normalize(path.resolve(file)); // normalize() arregla la ruta. resolve() la hace absoluta
+const colors = require('colors');
 
-const fs = require('fs');
-
-module.exports = () => {
-
+const detectedMd = (absolutePath) => { // Función para detectar archivos tipo .md
+  if (path.extname(absolutePath) === '.md') {
+    getLinks();
+  } else {
+    console.log('error. Ingrese un archivo .md');
+  }
 };
 
-const readFile = (path) => { // Función para leer archivo
-  let content = new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
+const getLinks = () => { // Función para obtener arreglo de todos los links
+  let printLinks = new Promise((resolve, reject) => {
+    readFile.readFile(absolutePath)
+      .then(datos => {
+        let renderer = new marked.Renderer();
+        let links = [];
+        renderer.link = function (href, title, text) {
+          links.push({
+            href: href,
+            text: text,
+            file: absolutePath,
+          });
+        };
+        marked(datos, {
+          renderer: renderer
+        });
+        links = filterHttp(links); // Filtrar por prefijo http
+        links = stateLinks(links)
+        return resolve(links)
+      })
+      .catch(err => {
+        (console.log(err));
+      })
   })
-  return content
+  return printLinks
 }
 
-module.exports.readFile = readFile;
+const stateLinks = (links) => { // Función que filtra por estado de links
+  links.forEach(element => {
+    fetch(element.href)
+      .then(response => {
+        if (response.status === 200) {
+          console.log(('File: '+ element.file + '\n'), colors.blue('Titulo: ' + element.text.toUpperCase() + '\n'), colors.yellow('href: ' + element.href + '\n'),  colors.green('Estado: ' + response.status + '\n'));
+        } else  {
+          console.log(('File: '+ element.file + '\n'), colors.blue('Titulo: ' + element.text.toUpperCase() + '\n'), colors.yellow('href: ' + element.href + '\n'),  colors.red('Estado: ' + response.status + '\n'));
+        }
+      })
+      .catch(error => 
+        console.log(colors.red('[X] Error en el Link: ' + element.href + '\n')))   
+  });
+  console.log(colors.green("Links Analizados: " + links.length));
+};
+
+const filterHttp = (links) => { // Función que filtra por prefijo http de links
+  let filterHttp = [];
+  links.forEach((element) => {
+    let prefix = element.href.substring(0, 4);
+    if (prefix == 'http') {
+      filterHttp.push(element);
+    }
+  })
+  return filterHttp;
+};
+
+detectedMd(absolutePath);
